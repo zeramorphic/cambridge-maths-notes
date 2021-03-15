39869556287f740c1fd1cce11d375809e1246876
thirdsgames <thirdsgames2018@gmail.com>
2021-03-15 19:52:34 +0000

    Automate construction of TikZ folders

---
+++ cmd/clean.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// cleanCmd represents the clean command
+var cleanCmd = &cobra.Command{
+	Use:   "clean",
+	Short: "Cleans up temporary build and output files",
+	Long:  "Cleans up temporary build and output files",
+	Run: func(cmd *cobra.Command, args []string) {
+		os.RemoveAll("build")
+		os.RemoveAll("tikz")
+
+		for _, file := range Files {
+			os.RemoveAll(file.FilePath + "/build")
+			os.RemoveAll(file.FilePath + "/tikz")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cleanCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cleanCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cleanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/setup.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// setupCmd represents the setup command
+var setupCmd = &cobra.Command{
+	Use:   "setup",
+	Short: "Sets up build output folders",
+	Long: `Sets up build output folders with the correct permissions
+so that latexmk has the correct privileges to write to the folders.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, file := range Files {
+			os.Mkdir(file.FilePath+"/build", 0777)
+			os.Mkdir(file.FilePath+"/tikz", 0777)
+		}
+
+		os.Mkdir("build", 0777)
+		os.Mkdir("tikz", 0777)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(setupCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// setupCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// setupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
